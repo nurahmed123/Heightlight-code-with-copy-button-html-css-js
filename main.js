7b88a89ba080767cc6fd8d569983b2ffac48d458
Nur Ahmed <84973115+nurahmed123@users.noreply.github.com>
2021-09-04 22:02:41 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // hljs.initHighlightingOnLoad();
   
     const codeBlock = document.getElementById('code');
     const copyButton = document.getElementById('copy-button');
@@ -8,30 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const copyTextHandler = () => {
       const text = codeBlock.innerText;
   
-      // first version - document.execCommand('copy');
-      // var element = document.createElement('textarea');
-      // document.body.appendChild(element);
-      // element.value = text;
-      // element.select();
-      // document.execCommand('copy');
-      // document.body.removeChild(element);
-  
-      // copySuccess.classList.add('show-message');
-      // setTimeout(() => {
-      //   copySuccess.classList.remove('show-message');
-      // }, 2500);
-  
-      //   second version - Clipboard API
       navigator.clipboard.writeText(text).then(
         () => {
           copyButton.innerHTML = "Copied!";
           setTimeout(() => {
             copyButton.innerHTML = "Copy";
           }, 1000);
-          // copySuccess.classList.add('show-message');
-          // setTimeout(() => {
-          //   copySuccess.classList.remove('show-message');
-          // }, 2500);
         },
         () => {
           console.log('Error writing to the clipboard');
